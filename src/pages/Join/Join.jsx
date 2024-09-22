067d02a4fcc1be7724bb9647c0a1cdc305db4a7b
freebob2b <freebob2b@gmail.com>
2024-09-22 22:38:47 +0900

    style: Improved responsiveness and added required

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,148 +1,69 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Join.module.css';
-import axios from 'axios';
-import Buttons from '../../component/Buttons/Buttons';
+import Button from '../../component/Button/Button';
 import Notice from '../../component/Notice.jsx';
-import { useRecoilValue } from 'recoil';
-import { apiUrlState } from '../../recoil/Atom.js';
-import { login } from '../../api/firebase.js';
+import { join, saveUser } from '../../api/firebase.js';
 import { useUserContext } from '../../context/userContext.jsx';
 
 const INITIAL_FORM = {
   email: '',
   pw: '',
-  name: '',
+  dogName: '',
 }
 
-export default function Join({ changeNotice }) {
+export default function Join() {
   const [form, setForm] = useState(INITIAL_FORM);
+  const { setUser } = useUserContext();
   const navigate = useNavigate();
-  const { user, setUser } = useUserContext();
 
   const handleInput = (e) => {
     const { id, value } = e.target;
     setForm({...form, [id]: value});
   }
 
-  const handleJoin = () => {
-    login(form, setUser)
-    .then(() => navigate('/'));
+  const handleJoin = (e) => {
+    e.preventDefault();
+    
+    join(form, setUser)
+    .then((uid) => {
+      saveUser(uid, form.dogName)
+      .then(() => navigate('/'))
+    });
   }
-    // const apiUrl = useRecoilValue(apiUrlState);
-    // const buttonName = {
-    //     cancel: '취소',
-    //     submit: '가입'
-    // }
-    // const cancelLink = {
-    //     path: '/login',
-    // }
-    // const userId = useRef();
-    // const userPw = useRef();
-    // const userName = useRef();
-    // const userArr = [userId, userPw, userName];
-    // const regExp = {
-    //     userId: [/^[a-z]+[a-z0-9]{4,}$/, 'ID 작성 : 영문, 숫자만 사용 가능(5~15자)'], 
-    //     userPw: [/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,}$/,
-    //             'PW 작성 : 영문과 숫자, 특수문자 최소 한가지 조합(8~15자)'],
-    //     userName: [/^[가-힣]{1,}$/, '이름 작성 : 한글 사용(1~10자)'],
-    // }
 
-    // function handleInfoRules(e) {
-    //     const getinputId = e.target.id;
-    //     const keys = Object.keys(regExp);
-    //     const keyIndex = keys.indexOf(getinputId);
-    //     const isRegExp = regExp[keys[keyIndex]][0].test(e.target.value);
-
-    //     if (isRegExp) {
-    //         e.target.setAttribute('regExp', true);
-    //         changeNotice('', '', 'none', 0);
-    //     } else {
-    //         e.target.setAttribute('regExp', false);
-    //         changeNotice(regExp[keys[keyIndex]][1], 'warning.png', 'flex', 0);
-    //     }
-    // }
-
-    // async function handleFormSubmit() {
-    //     const userInfo = {
-    //         userId: userId.current.value,
-    //         userPw: userPw.current.value,
-    //         userName: userName.current.value,
-    //     }
-    //     const condition = userInfo.userId === '' || userInfo.userPw === '' || userInfo.userName === '';
-
-    //     if(condition) {
-    //         changeNotice('모든 정보를 입력하세요', 'warning.png', 'flex', 0);
-    //         return;
-    //     }
-
-    //     const regExpBooleanArr = userArr.map((item) => {
-    //         return item.current.attributes.regExp.value;
-    //     })
-
-    //     if (regExpBooleanArr.includes("false")) {
-    //         changeNotice('정보를 규칙에 맞게 입력해주세요', 'warning.png', 'flex', 0);
-    //         return;
-    //     }
-
-    //     try {
-    //         const res = await axios.post(apiUrl + 'join', userInfo);
-    //         const data = await res.data;
-
-    //         if (data === 'Success') {
-    //             changeNotice('가입 성공', 'correct.png', 'flex', "/login");
-    //             return
-    //         }
-    //         if (data === 'Fail') {
-    //             changeNotice('이미 존재하는 ID 입니다', 'warning.png', 'flex', 0);
-    //         }
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // }
-    return (
-    //   <section className='column'>
-    //     <h2>회원가입</h2>
-    //     <Notice />
-    //     <form className={styles.form}>
-    //       <div>
-    //         <div>
-    //           <label className={styles.label} htmlFor='userId'>ID</label>
-    //           <input onChange={handleInfoRules} ref={userId} id='userId' type='text' maxLength='15'/>
-    //         </div>
-    //         <div>                       
-    //           <label className={styles.label} htmlFor='userPw'>PW</label>
-    //           <input onChange={handleInfoRules} ref={userPw} id='userPw' type='password' autoComplete="off" maxLength='15'/>
-    //         </div>
-    //         <div>
-    //           <label className={styles.label} htmlFor='userName'>이름</label>
-    //           <input onChange={handleInfoRules} ref={userName} id='userName' type='text' maxLength='10'/>
-    //         </div>
-    //       </div>
-    //     </form>
-    //     <Buttons buttonName={buttonName} cancelLink={cancelLink} handleFormSubmit={handleFormSubmit} disabled />
-    //   </section>
-
+  // 아이디가 이메일이 아닌 경우
+  // 이미 존재하는 아이디인 경우
+  // 비밀번호가 규칙에 어긋나는 경우
+  
+  return (
     <section className='column'>
       <h2>회원가입</h2>
       <Notice />
-      <form className={styles.form}>
-        <div>
+      <form className={styles.form} onSubmit={handleJoin}>
+        <div className={styles.inputWrap}>
           <div>
             <label className={styles.label} htmlFor='email'>이메일</label>
-            <input id='email' type='text' onChange={handleInput} />
+            <input className={styles.input} id='email' type='text'
+              onChange={handleInput} value={form.email} placeholder='이메일' required />
           </div>
           <div>                       
             <label className={styles.label} htmlFor='pw'>비밀번호</label>
-            <input id='pw' type='password' autoComplete="off" maxLength='15' onChange={handleInput} />
+            <input className={styles.input} id='pw' type='password' autoComplete="off"
+              maxLength='15' onChange={handleInput} value={form.pw} placeholder='비밀번호'
+              required />
           </div>
           <div>
-            <label className={styles.label} htmlFor='name'>이름</label>
-            <input id='name' type='text' maxLength='10' onChange={handleInput} />
+            <label className={styles.label} htmlFor='dogName'>반려견</label>
+            <input className={styles.input} id='dogName' type='text' maxLength='10'
+              onChange={handleInput} value={form.dogName} placeholder='반려견 이름' required />
           </div>
         </div>
+        <div className='buttonWrap'>
+          <Button destination='/login' name='취소' />
+          <Button isButton name='가입' />
+        </div>
       </form>
-      <Buttons buttonName={{submit: '가입', cancel: '취소'}} cancelLink='/login' handleJoin={handleJoin} />
     </section>
-    )
+  )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import { useUserContext } from '../../context/userContext';
+import Alert from "../../component/Alert/Alert";
 import styles from './Login.module.css';
 import Notice from '../../component/Notice.jsx';
-import { useRecoilValue } from 'recoil';
-import { apiUrlState } from '../../recoil/Atom.js';
 import { login } from '../../api/firebase.js';
-var store = require('store');
 
 const INITIAL_FORM = {
   email: '',
   pw: '',
 }
 
-export default function Login({ changeNotice }) {
+export default function Login() {
+  const { setUser } = useUserContext();
   const [form, setForm] = useState(INITIAL_FORM);
+  const [alert, setAlert] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const { setUser } = useState();
   const navigate = useNavigate();
 
   const handleInput = (e) => {
@@ -24,94 +23,50 @@ export default function Login({ changeNotice }) {
     setForm({...form, [id]: value});
   }
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
     setIsLoading(true);
 
     login(form, setUser)
     .then((isSuccess) => {
-      setIsLoading(false);
-
-      if (isSuccess) return navigate('/');
-      // 실패한 경우 알림 띄우기
+      if (isSuccess) {
+        return navigate('/');
+      } else {
+        setAlert('이메일 또는 비밀번호가 달라요!')
+      }
     })
+
+    setIsLoading(false);
   }
 
-    // const apiUrl = useRecoilValue(apiUrlState);
-    // const userId = useRef();
-    // const userPw = useRef();
-
-    // async function handleFormSubmit() {
-    //     const userInfo = {
-    //         userId: userId.current.value,
-    //         userPw: userPw.current.value
-    //     }
-    //     const condition = userInfo.userId === '' || userInfo.userPw === '';
-
-    //     if (condition) {
-    //         changeNotice('모든 정보를 입력하세요', 'warning.png', 'flex', 0);
-    //         return;
-    //     }
-
-    //     try {
-    //         const res = await axios.post(apiUrl + 'login', userInfo, { withCredentials: true });
-    //         const data = await res.data;
-
-    //         if (data === 'Success') {
-    //             store.set('loginState', 'true');
-    //             changeNotice('로그인 성공', 'correct.png', 'flex', "/");
-    //             return;
-    //         }
-
-    //         if (data === 'Fail_id') {
-    //             changeNotice('ID가 존재하지 않습니다', 'warning.png', 'flex', 0);
-    //             return;
-    //         }
-
-    //         if (data === 'Fail_pw') {
-    //             changeNotice('PW가 틀렸습니다', 'warning.png', 'flex', 0);
-    //         }
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // }
+  const handleAlert = () => {
+    setAlert('');
+  }
+  
   return (
-    // <section className='column'>
-    //     <Notice />
-    //     <form className={styles.form} method='post'>
-    //       <div>
-    //         <div>
-    //           <label className={styles.label} htmlFor='userId'>ID</label>
-    //           <input  ref={userId} id='userId' type='text' />
-    //         </div>
-    //         <div>
-    //           <label className={styles.label} htmlFor='userPw'>PW</label>
-    //           <input ref={userPw} id='userPw' type='password' autoComplete="off" />
-    //         </div>
-    //       </div>
-    //       <button className={styles.login} onClick={handleFormSubmit} type="submit">LOGIN</button>
-    //     </form>
-    //     <div className={styles.joinWrap}>
-    //       <Link className={styles.join} to="/join">회원가입 하러가기</Link>
-    //     </div>
-    // </section>
-  <section className='column'>
-    <Notice />
-    <form className={styles.form}>
-      <div>
-        <div>
-          <label className={styles.label} htmlFor='email'>ID</label>
-          <input id='email' type='text' onChange={handleInput} value={form.email} />
+    <section className='column'>
+      <Notice />
+      <form className={styles.form} onSubmit={handleLogin}>
+        <div className={styles.inputWrap}>
+          <div>
+            <label className={styles.label} htmlFor='email'>ID</label>
+            <input className={styles.input} id='email' type='text' onChange={handleInput}
+             value={form.email} placeholder='이메일' required />
+          </div>
+          <div>
+            <label className={styles.label} htmlFor='pw'>PW</label>
+            <input className={styles.input} id='pw' type='password' autoComplete="off"
+             onChange={handleInput} value={form.pw} placeholder='비밀번호' required />
+          </div>
         </div>
-        <div>
-          <label className={styles.label} htmlFor='pw'>PW</label>
-          <input id='pw' type='password' autoComplete="off" onChange={handleInput} value={form.pw}  />
-        </div>
+        <button className={styles.login} disabled={isLoading}>LOGIN</button>
+      </form>
+      <div className={styles.joinWrap}>
+        <Link className={styles.join} to="/join">회원가입 하러가기</Link>
       </div>
-      <button className={styles.login} onClick={handleLogin} type="submit" disabled={isLoading}>LOGIN</button>
-    </form>
-    <div className={styles.joinWrap}>
-      <Link className={styles.join} to="/join">회원가입 하러가기</Link>
-    </div>
-  </section>
+      {
+        alert && <Alert handleAlert={handleAlert} message={alert} />
+      }
+    </section>
   )
 }
