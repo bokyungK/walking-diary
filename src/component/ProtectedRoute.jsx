6b55df38bc6cd839097571ad88762dd96e11058a
freebob2b <freebob2b@gmail.com>
2024-09-25 22:57:38 +0900

    fix: Changing the position of the loading screen

--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useUserContext } from '../context/userContext';
-import { Navigate } from "react-router-dom";
-import Loading from './Loading/Loading';
+import { useNavigate } from "react-router-dom";
+import Alert from './Alert/Alert';
 
 export default function ProtectedRoute({ children }) {
   const { user } = useUserContext();
+  const navigate = useNavigate();
+  const handleAlert = () => {
+    navigate('/login', { replace: true });
+  }
 
-  if (user === undefined) {
-    return <Loading />
-  } else if (user === null) {
-    return <Navigate to='/login' replace />
+  if (user === null) {
+    return <Alert message='로그인이 필요해요!' handleAlert={handleAlert} />
   } else {
     return children;
   }
--- src/pages/Diaries/Diaries.jsx
+++ src/pages/Diaries/Diaries.jsx
@@ -2,22 +2,36 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { useUserContext } from '../../context/userContext.jsx';
-import styles from './Diaries.module.css';
 import { getDiaries } from "../../api/firebase.js";
+import styles from './Diaries.module.css';
+import Loading from '../../component/Loading/Loading';
 
 export default function Diaries() {
   const { user } = useUserContext();
-  const { data: diaries } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['diaries', user && user.uid],
     queryFn: () => getDiaries(user.uid),
     enabled: Boolean(user && user.uid),
     refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 10,
   })
+  const [diaries, setDiaries] = useState();
+  const [order, setOrder] = useState('최신 순서');
+  const handleOrder = (e) => setOrder(e.target.value)
 
-  // 여기 작업 필요
-  const [order] = useState();
-  const handleOrder = () => {};
+  useEffect(() => {
+    if (data) {
+      setDiaries(() => {
+        if (order === '최신 순서') {
+          return [...data].sort((a, b) => b.timestamp - a.timestamp);
+        } else if (order === '오래된 순서') {
+          return [...data].sort((a, b) => a.timestamp - b.timestamp);
+        } else if (order === '책갈피') {
+          return [...data].filter((item) => item.mark === true);
+        } 
+      })
+    }
+  }, [order, data])
 
   useEffect(() => {
     if (user) {
@@ -25,34 +39,43 @@ export default function Diaries() {
     }
   }, [user])
 
+  if (isLoading || diaries === undefined) return <Loading isInSection />
   return (
     <section className={`${styles.section} column`}>
       <h2>일기 보관함</h2>
-      <div className={styles.orderBox}>
-        <select className={styles.orderSelect} onChange={handleOrder} name='sort' value={order}>
-          <option>최신 순서</option>
-          <option>오래된 순서</option>
-          <option>책갈피</option>
-        </select>
-      </div>
-      <ul className={styles.diaryList}>
+      <div className={styles.diaryWrap}>
         {
-          diaries && diaries.map((item) => {
-            const { id, title, dog, imageUrl } = item;
-
-            return <Link className={styles.diaryLink} to={`/diary/${id}`} state={item} key={id}>
-              <li className={styles.diaryCard} key={id}>
-                <img className={styles.diaryImage} src={imageUrl} alt='산책 사진'/>
-                <div className={styles.diaryTitleWrap}>
-                  <p>{title} with {dog}</p>
-                </div>
-              </li>
-            </Link>
-          })
+          diaries && <>
+            <div className={styles.orderBox}>
+              <select className={styles.orderSelect} onChange={handleOrder} name='sort' value={order}>
+                <option>최신 순서</option>
+                <option>오래된 순서</option>
+                <option>책갈피</option>
+              </select>
+            </div>
+            <ul className={styles.diaryList}>
+              {
+                diaries.map((item) => {
+                  const { id, title, dog, imageUrl } = item;
+                  return <Link className={styles.diaryLink} to={`/diary/${id}`} state={item} key={id}>
+                    <li className={styles.diaryCard} key={id}>
+                      <img className={styles.diaryImage} src={imageUrl} alt='산책 사진'/>
+                      <div className={styles.diaryTitleWrap}>
+                        <p>{title} with {dog}</p>
+                      </div>
+                    </li>
+                  </Link>
+                })
+              }
+            </ul>
+          </>
         }
-      </ul>
+      </div>
       {
-        !diaries && <span>작성된 일기가 없습니다!</span>
+        !diaries && <div className={styles.noDiary}>
+          <p>작성된 일기가 없어요!</p>
+          <Link to='/diary/new'>일기 쓰러가기</Link>
+        </div>
       }
     </section>
   )
