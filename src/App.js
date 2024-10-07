ea227be0dac691856bda0af10bf92b24d82e365e
freebob2b <freebob2b@gmail.com>
2024-10-07 19:40:12 +0900

    refactor: Improved provider maintenance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './component/Header/Header.jsx';
-import { UserContextProvider } from './context/userContext.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UserContextProvider } from './context/userContext.jsx';
 import { AlertContextProvider } from './context/alertContext';
 import { SubmitContextProvider } from './context/submitContext.jsx';
 
 const queryClient = new QueryClient()
+const providers = [
+  UserContextProvider,
+  AlertContextProvider,
+  SubmitContextProvider
+]
+
+const combineProviders = (providers) => {
+  return ({ children }) => 
+    providers.reduceRight((AccumulatedComponents, CurrentProvider) => {
+      return (
+        <CurrentProvider>
+          {AccumulatedComponents}
+        </CurrentProvider>
+      );
+    }, children);
+};
+
+export const AppContextProvider = combineProviders(providers);
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <UserContextProvider>
-        <AlertContextProvider>
-          <SubmitContextProvider>
-            <Header />
-            <Outlet />
-          </SubmitContextProvider>
-        </AlertContextProvider>
-      </UserContextProvider>
+      <AppContextProvider>
+        <Header />
+        <Outlet />
+      </AppContextProvider>
     </QueryClientProvider>
   )
 }
