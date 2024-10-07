26a81bfcc97833541cd7bf037b736c7b434dd9d1
freebob2b <freebob2b@gmail.com>
2024-10-07 16:37:02 +0900

    feat: Add attendance check function

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -2,134 +2,103 @@ import React, { useState, useEffect } from 'react';
 import styles from './Banner.module.css';
 import { Link } from 'react-router-dom';
 import { useUserContext } from '../../context/userContext';
+import { useQuery } from '@tanstack/react-query';
+import { getDiaries } from "../../api/firebase.js";
 import Loading from '../../component/Loading/Loading';
+
+const DAY_LIST = ['일', '월', '화', '수', '목', '금', '토'];
+const TODAY = new Date();
+const YEAR = TODAY.getFullYear();
+const MONTH = TODAY.getMonth();
+const DATE = TODAY.getDate();
 
 function Banner() {
   const { user } =  useUserContext();
   const [calendar, setCalendar] = useState([]);
-  const [writedDate] = useState('');
-  const days = ['일', '월', '화', '수', '목', '금', '토'];
-  const today = new Date();
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth();
-
-  // useEffect(() => {
-  //   if (loginState === 'true') {
-  //     const fetchCalendar = async () => {
-  //       try {
-  //         // const res = await axios.get(apiUrl + 'calendar', { withCredentials: true })
-  //         const data = await res.data;
-
-  //         if (checkCookie(data, false)) {
-  //             return;
-  //         }
-
-  //         if (data === 'Nothing') {
-  //             return;
-  //         }
-
-  //         const writedArr = [];
-
-  //         data.forEach((item) => {
-  //             const year = parseInt(item.date.slice(0, 4));
-  //             const month = parseInt(item.date.slice(5, 7));
-  //             const date = parseInt(item.date.slice(8, 10));
-
-  //             if (currentYear === year && (currentMonth + 1) === month) {
-  //                 writedArr.push(date);
-  //             }
-  //             setWritedDate(writedArr);
-  //         })
-  //       } catch (err) {
-  //           console.error(err);
-  //       }
-  //     }
-      
-  //     fetchCalendar();
-  //   }
-  // }, [apiUrl, checkCookie, currentMonth, currentYear, loginState]);
+  const [writedList, setWritedList] = useState();
+  const { data } = useQuery({
+    queryKey: ['diaries', user && user.uid],
+    queryFn: () => getDiaries(user.uid),
+    enabled: Boolean(user && user.uid),
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 10,
+  })
 
   useEffect(() => {
-      const previousLastDate = new Date(currentYear, currentMonth, 0).getDate();
-      const currentFirstDay = new Date(currentYear, currentMonth, 1).getDay();
-      const currentLastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
-      const calendarLength = currentFirstDay === 0 ?
-                              // 1일이 일요일부터 시작할 때
-                              currentLastDate % 7 === 0 ? 
-                                  currentLastDate
-                                  :
-                                  currentLastDate + (7 - currentLastDate % 7) 
-                              :
-                              // 1일이 일요일부터 시작하지 않을 때
-                              (currentFirstDay + currentLastDate) % 7 === 0 ?
-                                  currentFirstDay + currentLastDate
-                                  :
-                                  currentFirstDay + currentLastDate + (7 - ((currentFirstDay + currentLastDate) % 7))
-      const dateArr = new Array(calendarLength);
-  
-      for (let i = 0; i < calendarLength; i++) {
-          const index = Math.floor(i / 7);
-          const date = (i + 1) - currentFirstDay;
-          const previousDate = previousLastDate - currentFirstDay + (i + 1);
-          const num = i < currentFirstDay ? 
-            currentMonth === 0 ? `12/${previousDate}` : `${currentMonth}/${previousDate}` 
-            :
-            date > currentLastDate ?
-                currentMonth === 11 ? `1/${date - currentLastDate}` : `${currentMonth + 2}/${date - currentLastDate}`
-                :
-                date;
+    const lastDate = new Date(YEAR, MONTH + 1, 0).getDate();
+    const calendarLen = (Math.floor(lastDate / 7) + (lastDate % 7 > 0 ? 1 : 0)) * 7;
+    const firstDay = new Date(YEAR, MONTH, 1).getDay();
+    const calendarList = []
 
-          if (dateArr[index] === undefined) {
-              dateArr[index] = [num, ]
-          } else {
-              dateArr[index].push(num)
-          }
+    for (let i = 0; i < calendarLen; i++) {
+      if (i < firstDay && i === 0) {
+        TODAY.setDate(TODAY.getDate() - DATE - firstDay + 1);
+      } else {
+        TODAY.setDate(TODAY.getDate() + 1);
       }
-      setCalendar(dateArr);
-  }, [currentMonth, currentYear])
+
+      if (i % 7 === 0) {
+        calendarList.push([TODAY.getDate()])
+      } else {
+        const listIdx = Math.floor(i / 7);
+        calendarList[listIdx].push(TODAY.getDate());
+      }
+    }
+
+    setCalendar(calendarList);
+  }, [])
+
+  useEffect(() => {
+    if (data) {
+      setWritedList(data.map((item) => {
+        const { time } = item;
+        const month = time.slice(5, 7);
+        const date = parseInt(time.slice(8, 10));
+        const isCurMonth = MONTH === parseInt(month);
+        return isCurMonth ? `${month}/${date}` : date;
+      }))
+    }
+  }, [data])
 
   return (
       <section className={`${styles.section} column`}>
         {
           user &&
-              <div>
-                  <h2>{ `${currentYear}년 ${currentMonth + 1}월 출석` }</h2>
-                  <table className={styles.table}>
-                      <thead className={styles.thead}>
-                          <tr>
-                              {
-                                  days.map((day) => {
-                                      return <th className={styles.th} key={`day-${day}`}>{day}</th>;
-                                  })
-                              }
-                          </tr>
-                      </thead>
-                      <tbody className={styles.tbody}>
-                          {
-                              calendar.map((arr, idx) => {
-                                  return <tr className={styles.rows} key={`week-${idx + 1}`}>
-                                      {
-                                          arr.map((num) => {
-                                            const today = new Date();
-                                            const todayDate = today.getDate();
-                                            const todayClassName = num === todayDate && styles.today;
-                                            
-                                            return typeof num === 'string' ? 
-                                            <td className={`${styles.td} ${styles.otherMonth} ${todayClassName}`} key={`current-${num}`}>&nbsp;{num}</td>
-                                            :
-                                            <td className={`${styles.td} ${todayClassName}`}  key={`current-${num}`}>{ writedDate.includes(num) ?
-                                                <>&nbsp;{num}<img src='/attendance.png' alt={num} /></>
-                                                :
-                                                <>&nbsp;{num}</>
-                                            }</td>
-                                          })
-                                      }
-                                  </tr>;
-                              })
+          <div>
+            <h2>{ `${YEAR}년 ${MONTH + 1}월 출석` }</h2>
+            <table className={styles.table}>
+              <thead className={styles.thead}>
+                <tr>
+                  { 
+                    DAY_LIST.map((day) => {
+                        return <th className={styles.th} key={`day-${day}`}>{day}</th>;
+                    })
+                  }
+                </tr>
+              </thead>
+              <tbody className={styles.tbody}>
+                {
+                  calendar.map((arr, idx) => {
+                  return <tr className={styles.rows} key={`week-${idx + 1}`}>
+                    {
+                      arr.map((num) => {
+                        const todayClass = num === DATE && styles.today;
+                        const otherMonthClass = ((idx === 0 && num > 7) || (idx === calendar.length - 1 && num < 7)) && styles.otherMonth;
+                      
+                        return <td className={`${styles.td} ${otherMonthClass} ${todayClass}`} key={`current-${num}`}>
+                          <span>{num}</span>
+                          { writedList && writedList.includes(num) && 
+                            <img className={styles.attendance} src='/icons/footprint.png' alt={num} />
                           }
-                      </tbody>
-                  </table>
-              </div>
+                        </td>
+                      })
+                    }
+                  </tr>;
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         }
         {
           user === null   &&
@@ -150,4 +119,3 @@ function Banner() {
   }
   
   export default Banner;
-
